8427dc9d6bc5cfc19f16dd05b093bd06a0d0f42e
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-04-15 00:22:29 +0900

    initial commit

---
+++ src/main/java/advpro/b2/rasukanlsp/RasukanLSPApplication.java
@@ -0,0 +1,13 @@
+package advpro.b2.rasukanlsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasukanLSPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RasukanLSPApplication.class, args);
+    }
+
+}
