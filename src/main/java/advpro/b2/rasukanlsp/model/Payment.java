71fa20cb27b6cdf7a2bb535e64472cba2d4f44b9
syazantri <syazantri@gmail.com>
2024-05-01 20:44:19 +0700

    [REFACTOR] Change design pattern payment to builder

--- src/main/java/advpro/b2/rasukanlsp/model/Payment.java
+++ src/main/java/advpro/b2/rasukanlsp/model/Payment.java
@@ -1,34 +1,80 @@
 package advpro.b2.rasukanlsp.model;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
-import java.util.List;
-import java.util.Map;
+import jakarta.persistence.*;
+
 import java.util.UUID;
 
-@Setter
+@Entity
+@Table(name = "payments")
 public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Getter
+    @Column(name = "user_id", nullable = false)
     private String userId;
 
-    private String orderId;
+    @OneToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
 
-    private Long nominal;
+    @Column(name = "nominal", nullable = false)
+    private int nominal;
 
+    @Column(name = "payment_status", nullable = false)
     private String paymentStatus;
 
+    @Column(name = "payment_status", nullable = true)
+    private String paymentNotes;
+
+    @Column(name = "discount", nullable = true)
+    private int discount;
+
+    // Constructor
     public Payment() {}
 
-    public Payment(UUID id, String userId, String orderId, Long nominal, String paymentStatus) {
+    // Payment pakai diskon dan notes
+    public Payment(UUID id, String userId, Order order, int nominal, String paymentStatus, int discount, String paymentNotes) {
         this.id = id;
         this.userId = userId;
-        this.orderId = orderId;
+        this.order = order;
+        this.nominal = nominal;
+        this.paymentStatus = paymentStatus;
+        this.discount = discount;
+        this.paymentNotes = paymentNotes;
+
+        validateAttribute();
+    }
+
+    // Payment pakai notes tp gapakai diskon
+    public Payment(UUID id, String userId, Order order, int nominal, String paymentStatus, String paymentNotes) {
+        this.id = id;
+        this.userId = userId;
+        this.order = order;
+        this.nominal = nominal;
+        this.paymentStatus = paymentStatus;
+        this.paymentNotes = paymentNotes;
+
+        validateAttribute();
+    }
+
+    // Payment pakai diskon tp gapakai notes
+    public Payment(UUID id, String userId, Order order, int nominal, String paymentStatus, int discount) {
+        this.id = id;
+        this.userId = userId;
+        this.order = order;
+        this.nominal = nominal;
+        this.paymentStatus = paymentStatus;
+        this.discount = discount;
+
+        validateAttribute();
+    }
+    // Payment gapakai diskon dan notes
+    public Payment(UUID id, String userId, Order order, int nominal, String paymentStatus) {
+        this.id = id;
+        this.userId = userId;
+        this.order = order;
         this.nominal = nominal;
         this.paymentStatus = paymentStatus;
 
@@ -38,31 +84,29 @@ public class Payment {
     public String getId() {
         return id.toString();
     }
-
-    public String getOrderId() {
-        return orderId;
+    public Order getOrder() { return order; }
+    public String getUserId() { return userId.toString(); }
+    public int getNominal() {
+        return nominal;
     }
-
-    public String nominal() {
-        return nominal.toString();
-    }
-
     public String getPaymentStatus() {
         return paymentStatus;
     }
+    public String getPaymentNotes() { return paymentNotes; }
+    public int getDiscount() {return discount; }
 
     public void validateAttribute() {
-        if (userId == null || userId.isEmpty()) {
-            throw new IllegalArgumentException("User ID cannot be null or empty.");
-        }
-        if (orderId == null || orderId.isEmpty()) {
-            throw new IllegalArgumentException("Order ID cannot be null or empty.");
-        }
-        if (nominal == null) {
-            throw new IllegalArgumentException("Nominal cannot be null or empty.");
-        }
-        if (!paymentStatus.equals("PENDING") && !paymentStatus.equals("ACCEPTED") && !paymentStatus.equals("REJECTED")) {
-            throw new IllegalArgumentException("Invalid status.");
-        }
+//        if (userId == null || userId.isEmpty()) {
+//            throw new IllegalArgumentException("User ID cannot be null or empty.");
+//        }
+//        if (orderId == null || orderId.isEmpty()) {
+//            throw new IllegalArgumentException("Order ID cannot be null or empty.");
+//        }
+//        if (nominal == null) {
+//            throw new IllegalArgumentException("Nominal cannot be null or empty.");
+//        }
+//        if (!paymentStatus.equals("PENDING") && !paymentStatus.equals("ACCEPTED") && !paymentStatus.equals("REJECTED")) {
+//            throw new IllegalArgumentException("Invalid status.");
+//        }
     }
 }
---
+++ src/main/java/advpro/b2/rasukanlsp/model/builder/PaymentBuilder.java
@@ -0,0 +1,47 @@
+package advpro.b2.rasukanlsp.model.builder;
+
+import advpro.b2.rasukanlsp.model.Payment;
+
+import java.util.UUID;
+
+public class PaymentBuilder {
+    private UUID id;
+    private UUID orderId;
+    private UUID userId;
+    private Long nominal;
+    private String paymentStatus;
+    private String paymentNotes;
+    private int discount;
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public void setNominal(Long nominal) {
+        this.nominal = nominal;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public void setPaymentNotes(String paymentNotes) {
+        this.paymentNotes = paymentNotes;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public Payment build() {
+        return new Payment();
+    }
+}
