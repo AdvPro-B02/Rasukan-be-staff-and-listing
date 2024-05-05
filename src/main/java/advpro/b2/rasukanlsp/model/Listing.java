61fc15767b8f2158804a411164450a5eb6b295ac
syazantri <syazantri@gmail.com>
2024-05-05 19:11:01 +0700

    [GREEN] Add implementation for models

---
+++ src/main/java/advpro/b2/rasukanlsp/model/Listing.java
@@ -0,0 +1,40 @@
+package advpro.b2.rasukanlsp.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+
+@Getter @Setter
+public class Listing {
+    private UUID listingId;
+    private String name;
+    private int price;
+    private int stock;
+    private UUID seller;
+
+    public Listing(UUID listingId, String name, int stock, int price, UUID seller){
+        this.listingId = listingId;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.seller = seller;
+    }
+
+    public Listing() {}
+
+    @Override
+    public String toString() {
+        ObjectMapper om = new ObjectMapper();
+        try {
+            return om.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
