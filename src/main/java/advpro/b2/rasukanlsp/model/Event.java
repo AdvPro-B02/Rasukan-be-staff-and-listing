82422484390c07bd1bbb2b771a225a59509d7b9d
syazantri <syazantri@gmail.com>
2024-05-05 19:26:34 +0700

    [REFACTOR] Delete unnecessary files

--- src/main/java/advpro/b2/rasukanlsp/model/Event.java
+++
@@ -1,4 +0,0 @@
-package advpro.b2.rasukanlsp.model;
-
-public interface Event {
-}
--- src/main/java/advpro/b2/rasukanlsp/model/TopupStatusChangeEvent.java
+++
@@ -1,9 +0,0 @@
-package advpro.b2.rasukanlsp.model;
-
-import java.util.UUID;
-import lombok.Getter;
-
-public class TopupStatusChangeEvent implements Event {
-    private UUID topupId;
-    private String newStatus;
-}
