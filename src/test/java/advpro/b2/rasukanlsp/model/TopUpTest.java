2084e55fd142baed7a8fe149a008ed6a6ddcd897
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-05-08 11:24:29 +0900

    [RED] Create tests for TopUp model

---
+++ src/test/java/advpro/b2/rasukanlsp/model/TopUpTest.java
@@ -0,0 +1,46 @@
+package advpro.b2.rasukanlsp.model;
+
+import advpro.b2.rasukanlsp.enums.TopUpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TopUpTest {
+    
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void testCreateTopUpDefaultStatusPending() {
+        UUID id = UUID.randomUUID();
+        UUID user = UUID.randomUUID();
+        int amount = 100000;
+        TopUp topUp = new TopUp(id, user, amount);
+
+        assertEquals(id, topUp.getId());
+        assertEquals(user, topUp.getUser());
+        assertEquals(amount, topUp.getAmount());
+        assertEquals(TopUpStatus.PENDING, topUp.getStatus());
+    }
+
+    @Test
+    void testSetStatus_success() {
+        UUID id = UUID.randomUUID();
+        UUID user = UUID.randomUUID();
+        int amount = 100000;
+        TopUp topUp = new TopUp(id, user, amount);
+
+        assertEquals(TopUpStatus.PENDING, topUp.getStatus());
+
+        topUp.setStatus(TopUpStatus.ACCEPTED);
+
+        assertEquals(TopUpStatus.ACCEPTED, topUp.getStatus());
+
+        topUp.setStatus(TopUpStatus.REJECTED);
+
+        assertEquals(TopUpStatus.REJECTED, topUp.getStatus());
+    }
+}
