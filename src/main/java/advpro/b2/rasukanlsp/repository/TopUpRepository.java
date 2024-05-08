1801e755b85eac380c4d10b01de89cc492c5dc44
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-05-08 21:51:10 +0900

    [GREEN] Implement TopUpRepository to pass tests

---
+++ src/main/java/advpro/b2/rasukanlsp/repository/TopUpRepository.java
@@ -0,0 +1,13 @@
+package advpro.b2.rasukanlsp.repository;
+
+import advpro.b2.rasukanlsp.model.TopUp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TopUpRepository extends JpaRepository<TopUp, UUID> {
+    List<TopUp> findByUser(UUID user);
+}
